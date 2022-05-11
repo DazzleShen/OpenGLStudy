7aca72c5848b83e7101a5916d4c4665611898e57
小火三尧 <876752404@qq.com>
2022-05-11 20:45:00 +0800

    add markdown note

--- OpenGLStudy/OpenGLStudy/Shader.cpp
+++ OpenGLStudy/OpenGLStudy/Shader.cpp
@@ -2,7 +2,5 @@
 
 void Shader() 
 {
-	int nrAttributes = 0;
-	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
-	std::cout << "Maximum nr of vartex attributes supported: " << nrAttributes << std::endl;
+
 }
